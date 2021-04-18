343f38e686f17067ff7b8ef0c9093422f9c47547
Lucas Wagner <you@example.com>
2021-04-18 16:25:12 +0200

    Maven Backup Setup

---
+++ backend/gamingdate/src/main/java/com/gamingdate/gamingdate/GamingdateApplication.java
@@ -0,0 +1,13 @@
+package com.gamingdate.gamingdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingdateApplication.class, args);
+	}
+
+}
---
+++ backend/gamingdate/src/test/java/com/gamingdate/gamingdate/GamingdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamingdate.gamingdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
