9a41dce90235b57bfd45040d4c39cb7d73970101
simonmtr <294simon@googlemail.com>
2021-04-19 11:46:38 +0200

    added backend controller for testing

--- backend/gamingdate/src/main/java/com/gamingdate/gamingdate/HelloWorldController.java
+++ backend/gamingdate/src/main/java/com/gamingdate/gamingdate/HelloWorldController.java
@@ -1,2 +1,22 @@
-package main.java.com.gamingdate.gamingdate;public class HelloWorldController {
+package com.gamingdate.gamingdate;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200") // needed to be able to call from flutter web
+    @PostMapping("/accountdata")
+    public String index(@RequestBody int id) {
+        System.out.println(id);
+        if (id == 0) {
+            return "id was 0";
+        }
+        return "id was not 0";
+
+    }
 }
